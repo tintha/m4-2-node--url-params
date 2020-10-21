aaea20ace297e3ea741adfd86f3c557968fa189c
tintha <tintha@gmail.com>
2020-10-21 19:39:28 -0400

    Ex 1-6

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@
 // import the needed node_modules.
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const { top50 } = require('./data/top50');
 
 express()
   // Below are methods that are included in express(). We chain them for convenience.
@@ -18,7 +20,83 @@ express()
   // Nothing to modify above this line
   // ---------------------------------
   // add new endpoints here 👇
+  .get('/top50', (req, res) => {
+    const data = top50;
+    res.status(200).json({ 
+      status: 200, data
+    });
+  })
 
+  .get('/top50/song/:rank', (req, res) => {
+    const songRank = req.params;
+    const rankNum = Number(songRank.rank);
+    const data = top50.find(song => song.rank === rankNum);
+    if (!data) {
+      res.status(400).json({
+        status: 400, 
+        message: "Song not found."
+      })
+      
+    } else {
+      res.status(200).json({
+        status: 200, data
+      })
+    }
+  })
+
+  .get('/top50/artist/:artist', (req, res) => {
+    const artistParams = req.params;
+    const searchedArtist = artistParams.artist.toLowerCase();
+    // what to do with spaces, dashes..??
+    const data = top50.filter((song) => 
+      song.artist.toLowerCase().includes(searchedArtist)
+    );
+    if (data.length == 0) {
+      res.status(400).json({
+        status: 400,
+        message: "Artist not found."
+      })
+    } else {
+      res.status(200).json({
+        status: 200, data
+      })
+    }
+  })
+
+  .get('/top50/popular-artist', (req, res) => {
+    const artistArray = top50.map((song) => song.artist);
+    let topArtist = "";
+    const countArtist = artistArray.reduce(function (acc, curr) {
+      let topArtistCount = 1;
+      if (typeof acc[curr] == 'undefined') {
+        acc[curr] = 1;
+      } else {
+        acc[curr] += 1;
+        topArtistCount += 1;
+        if (acc[curr] > topArtistCount) {
+          topArtist = curr;
+        }
+      }
+      return acc;
+      }, {});
+      const top50copy = [...top50];
+      const data = top50copy.filter((song) => song.artist == topArtist);
+    res.status(200).json({
+      status: 200, data
+    })
+  })
+
+  .get('/top50/artist', (req, res) => {
+    const data = [];
+    const artistArray = top50.map((song) => song.artist);
+    function artistSet (value, set) {
+      data.push(value);
+    }
+    new Set(artistArray).forEach(artistSet);
+    res.status(200).json({
+      status: 200, data
+    })
+  })
   // add new endpoints here ☝️
   // ---------------------------------
   // Nothing to modify below this line
